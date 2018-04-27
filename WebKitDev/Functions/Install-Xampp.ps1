b982ae6a3859fb8bfd63b1c817eb05dc95cd2f5d
Don <don.j.olmstead@gmail.com>
2018-04-27 15:45:23 -0700

    feat(): Add xampp install

---
+++ WebKitDev/Functions/Install-Xampp.ps1
@@ -0,0 +1,38 @@
+# Copyright (c) 2018, the WebKit for Windows project authors.  Please see the
+# AUTHORS file for details. All rights reserved. Use of this source code is
+# governed by a BSD-style license that can be found in the LICENSE file.
+
+<#
+  .Synopsis
+  Installs XAMPP.
+
+  .Description
+  Downloads the specified release of Ruby and installs it silently on the host.
+
+  A silent install of XAMPP does not allow a path to be specified so the install
+  will always be present at C:/xampp.
+
+  .Parameter Version
+  The version of XAMPP to install.
+
+  .Example
+    # Install 7.2.4
+    Install-Xampp -Version 7.2.4
+#>
+Function Install-Xampp {
+  Param(
+    [Parameter(Mandatory)]
+    [string] $version
+  )
+
+  $url = ('https://www.apachefriends.org/xampp-files/{0}/xampp-win32-{0}-0-VC15-installer.exe' -f $version);
+  
+  $options = @(
+    '--unattendedmodeui', 'none',
+    '--mode', 'unattended',
+    '--launchapps', '0',
+    '--disable-components', 'xampp_mysql,xampp_filezilla,xampp_mercury,xampp_tomcat,xampp_perl,xampp_phpmyadmin,xampp_webalizer,xampp_sendmail'
+  );
+
+  Install-FromExe -Name 'xampp' -Url $url -Options $options -NoVerify;
+}
