5694968c2f10f01d5aeae1c2c4df96c235e411b2
Don <don.j.olmstead@gmail.com>
2018-05-01 17:10:26 -0700

    feat(make): Add make support

---
+++ WebKitDev/Functions/Install-Make.ps1
@@ -0,0 +1,43 @@
+# Copyright (c) 2018, the WebKit for Windows project authors.  Please see the
+# AUTHORS file for details. All rights reserved. Use of this source code is
+# governed by a BSD-style license that can be found in the LICENSE file.
+
+<#
+  .Synopsis
+  Installs make.
+
+  .Description
+  Downloads the specified release of make and unzips it to the specified
+  location on disk.
+
+  Before installation `Register-SystemPath` should be used to add the install
+  location to the system path.
+
+  .Link Register-SystemPath
+
+  .Parameter Version
+  The version of make to install.
+
+  .Parameter InstallationPath
+  The path to install at.
+
+  .Example
+    # Install 3.81
+    Install-Make -Version 3.81 -InstallationPath C:\gnuwin32
+#>
+Function Install-Make {
+  Param(
+    [Parameter(Mandatory)]
+    [string] $version,
+    [Parameter(Mandatory)]
+    [string] $installationPath
+  )
+
+  $url = ('https://downloads.sourceforge.net/project/gnuwin32/make/{0}/make-{0}-bin.zip' -f $version);
+
+  Install-FromArchive -Name 'make' -Url $url -InstallationPath $installationPath -NoVerify;
+
+  $depsUrl = ('https://downloads.sourceforge.net/project/gnuwin32/make/{0}/make-{0}-dep.zip' -f $version);
+
+  Install-FromArchive -Name 'make' -Url $depsUrl -InstallationPath $installationPath  
+}
