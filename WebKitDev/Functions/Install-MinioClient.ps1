2b8315200ed32550918f05b301710ef0ce78b19d
Don <don.j.olmstead@gmail.com>
2019-11-20 18:54:53 -0800

    feat(): Add Minio Client install

---
+++ WebKitDev/Functions/Install-MinioClient.ps1
@@ -0,0 +1,41 @@
+# Copyright (c) 2019, the WebKit for Windows project authors.  Please see the
+# AUTHORS file for details. All rights reserved. Use of this source code is
+# governed by a BSD-style license that can be found in the LICENSE file.
+
+<#
+  .Synopsis
+  Installs the Minio client manager.
+
+  .Description
+  Downloads the latest release of Minio Client and places it in the specified
+  location on disk.
+
+  .Parameter InstallationPath
+  The path to install at.
+
+  .Example
+    # Install
+    Install-MinioClient-InstallationPath C:\Minio
+#>
+Function Install-MinioClient {
+    Param(
+        [Parameter(Mandatory)]
+        [string] $installationPath,
+        [Parameter()]
+        [switch] $noPath = $false
+    )
+
+    $url = 'https://dl.min.io/client/mc/release/windows-amd64/mc.exe';
+
+    if (!$noPath) {
+        # Minio Client installs an exe in the root
+        Register-SystemPath $installationPath;
+    }
+
+    Install-FromExeDownload `
+        -Name 'mc' `
+        -Url $url `
+        -InstallationPath $installationPath `
+        -NoVerify:$noPath `
+        -VersionOptions @('version');
+}
