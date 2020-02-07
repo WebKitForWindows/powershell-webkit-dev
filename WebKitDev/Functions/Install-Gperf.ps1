2821d3367a8dbc9e3595f354dcf0d2fc51aeaec5
foopoiuyt <github@zombiestormtrooper.com>
2020-02-06 16:03:17 -0800

    Use gnuwin32 version of gperf and diff due to line ending issues (#12)

--- WebKitDev/Functions/Install-Gperf.ps1
+++ WebKitDev/Functions/Install-Gperf.ps1
@@ -7,8 +7,33 @@
   Installs gperf.
 
   .Description
-  Installs the latest version of gperf provided by MSYS2.
+  Downloads the specified release of gperf and unzips it to the specified
+  location on disk.
+
+  Before installation `Register-SystemPath` should be used to add the install
+  location to the system path.
+
+  .Link Register-SystemPath
+
+  .Parameter Version
+  The version of gperf to install.
+
+  .Parameter InstallationPath
+  The path to install at.
+
+  .Example
+    # Install 3.0.1
+    Install-Gperf -Version 3.0.1 -InstallationPath C:\gnuwin32
 #>
 Function Install-Gperf {
-    Install-FromPacman -Name 'gperf'
+    Param(
+        [Parameter(Mandatory)]
+        [string] $version,
+        [Parameter(Mandatory)]
+        [string] $installationPath
+    )
+
+    $url = ('https://newcontinuum.dl.sourceforge.net/project/gnuwin32/gperf/{0}/gperf-{0}-bin.zip' -f $version);
+
+    Install-FromArchive -Name 'gperf' -Url $url -InstallationPath $installationPath;
 }
