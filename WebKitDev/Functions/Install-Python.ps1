d455acaeff081a17a819b50ec2cf295032c32b17
foopoiuyt <github@zombiestormtrooper.com>
2020-03-17 08:47:14 -0700

    Support python 3 from Install-Python (#13)
    Co-authored-by: Stephan Szabo <stephan.szabo@sony.com>

--- WebKitDev/Functions/Install-Python.ps1
+++ WebKitDev/Functions/Install-Python.ps1
@@ -37,22 +37,35 @@ Function Install-Python {
     $major, $minor, $patch = $version.split('.');
 
     if ($major -ne '2') {
-        Write-Error 'Only Python version 2.x is usable for WebKit development';
-        return;
+        $pythonUrl = ('https://www.python.org/ftp/python/{0}/python-{0}-amd64.exe' -f $version);
+
+        $options = @(
+            '/quiet',
+            'InstallAllUsers=1',
+            'PrependPath=1',
+            'AssociateFiles=0'
+        );
+
+        if ($installationPath) {
+            $options += ('TargetDir="{0}"' -f $installationPath)
+        }
+
+        Install-FromExe -Name 'python' -Url $pythonUrl -Options $options;
     }
+    else {
+        $pythonUrl = ('https://www.python.org/ftp/python/{0}/python-{0}.amd64.msi' -f $version);
 
-    $pythonUrl = ('https://www.python.org/ftp/python/{0}/python-{0}.amd64.msi' -f $version);
+        $options = @(
+            'ALLUSERS=1',
+            'ADDLOCAL=DefaultFeature,Extensions,TclTk,Tools,PrependPath'
+        );
 
-    $options = @(
-        'ALLUSERS=1',
-        'ADDLOCAL=DefaultFeature,Extensions,TclTk,Tools,PrependPath'
-    );
+        if ($installationPath) {
+            $options += ('TARGETDIR="{0}"' -f $installationPath);
+        }
 
-    if ($installationPath) {
-        $options += ('TARGETDIR="{0}"' -f $installationPath);
+        Install-FromMsi -Name 'python' -Url $pythonUrl -Options $options;
     }
-
-    Install-FromMsi -Name 'python' -Url $pythonUrl -Options $options;
 
     # Install PIP
     $pipInstall = ('pip=={0}' -f $pipVersion);
@@ -62,8 +75,10 @@ Function Install-Python {
     python get-pip.py $pipInstall;
     Remove-Item get-pip.py -Force;
 
-    # Install Visual Studio for Python 2.7
-    $vcForPythonUrl = 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi';
+    if ($major -eq '2') {
+        # Install Visual Studio for Python 2.7
+        $vcForPythonUrl = 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi';
 
-    Install-FromMsi -Name 'VCForPython27' -Url $vcForPythonUrl -NoVerify;
+        Install-FromMsi -Name 'VCForPython27' -Url $vcForPythonUrl -NoVerify;
+    }
 }
